c3cf72cc993e0b59db98c2f8fa3e68913c25f3b0
Nathan Trujillo <nathan.trujillo@intensity.com>
2017-01-11 08:38:30 -0800

    try to integrate library

--- index.js
+++ index.js
@@ -1,15 +1,74 @@
 var SerialPort = require('serialport');
+
+var PixelPusher = require('heroic-pixel-pusher');
+PixelPusherInst = new PixelPusher;
+
+
+console.log(PixelPusher);
+
+
+var fs = require('fs');
+var imageFiles;
+var currentFile = 0;
 var teensyPort;
-SerialPort.list(function (err, ports) {
-  ports.forEach(function(port) {
-    if (/usb.+[0-9]+/.test(port.comName)) {
-      console.log('Found:', port);
-      teensyPort = new SerialPort(port.comName, {
-	parser: SerialPort.parsers.readline('\n')
-      });
-      teensyPort.on('data', function (data) {
-        console.log(data);
-      });
-    }
+
+var RPM;
+var rpmReg = new RegExp("\(smoothed\): ([0-9.]+)", "g");
+var tickReg = new RegExp("TICK", "g");
+
+function loadFiles() {
+  var items = fs.readdirSync('./data/');
+  console.log(items);
+  imageFiles = items.filter(item => {
+    return /\.(jpg|jpeg|png)$/i.test(item);
   });
-});
+  imageFiles.forEach( (item, i) => {
+    items[i] = './data/' + items[i];
+  });
+}
+
+function nextFile() {
+  currentFile++;
+  currentFile = (currentFile > imageFiles.length) ? 0 : currentFile;
+}
+
+function previousFile() {
+  currentFile--;
+  currentFile = (currentFile < 0) ? imageFiles.length : currentFile;
+}
+
+function pickupTeensy () {
+  SerialPort.list(function (err, ports) {
+    ports.forEach(function(port) {
+      if (/usb.+[0-9]+/.test(port.comName)) {
+        console.log('Found teensy:', port);
+        teensyPort = new SerialPort(port.comName, {
+  	       parser: SerialPort.parsers.readline('\n')
+        });
+        teensyPort.on('data', (data) => {
+          console.log(data);
+          var matches = rpmReg.exec(data);
+          if (matches[1]) {
+            RPM = matches[1];
+            console.log('\t\tRPM', RPM);
+          }
+          matches = tickReg.exec(data);
+          if (matches[1]) {
+            console.log('SYNC signal caught.', new Date());
+          }
+        });
+        teensyPort.on('close', () => {
+          teensyPort = undefined;
+          console.log('Teensy disconnected.');
+        });
+      }
+    });
+  });
+}
+
+setInterval(function () {
+  if (teensyPort == undefined) {
+    console.log('Looking for Teensy..');
+    pickupTeensy();
+  }
+}, 1000);
