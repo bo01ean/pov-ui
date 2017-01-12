bdf98c37efae4d9bdd8f99ed761f14435be327d3
Nathan Trujillo <NathanTrujillo@shop.local>
2017-01-11 19:00:50 -0800

    Make the real interface for crying out loud..

--- index.js
+++ index.js
@@ -1,10 +1,100 @@
 var SerialPort = require('serialport');
-
 var PixelPusher = require('heroic-pixel-pusher');
-PixelPusherInst = new PixelPusher;
+var debug = require('boolean-debug')('info');
+var noise = require('boolean-debug')('noise');
+
+var PixelInterface = function () {
+
+  var vm = this;
+
+  vm.PixelStrip = PixelPusher.PixelStrip;
+  vm.PixelPusherInstance = new PixelPusher(); // will start listener.
+
+  vm.UPDATE_FREQUENCY_MILLIS = 100;
+  vm.PIXELS_PER_STRIP = 1;
+  vm.strip = new vm.PixelStrip(0, vm.PIXELS_PER_STRIP);
+  vm.exec = function () {}; // NOP
+  vm.timer = null;
+
+  vm.updateTiming = function (timing) {
+    if (typeof timing == 'number') {
+      debug('Setting new timing to:', timing, 'ms');
+      vm.UPDATE_FREQUENCY_MILLIS = timing;
+      vm.resetTimer();
+    }
+  };
+  vm.updateExecutable = function (exec) {
+    if (typeof exec == 'function') {
+      debug('Updating exec');
+      vm.exec = exec;
+      vm.resetTimer();
+    }
+  }
+
+  vm.resetTimer = function () {
+    if (vm.timer !== null) {
+      debug('Killing timer');
+      clearTimeout(vm.timer);
+    }
+
+    debug('Restarting loop');
+    vm.timer = setInterval(function() {
+      vm.exec();
+    }, vm.UPDATE_FREQUENCY_MILLIS);
+
+  }
+
+  vm.updateStrip = function (pixels) {
+    vm.PIXELS_PER_STRIP = pixels;
+    vm.strip = new vm.PixelStrip(0, vm.PIXELS_PER_STRIP);
+  }
+
+  vm.PixelPusherInstance.on('discover', (controller) => {
+
+    var info = ['-----------------------------------',
+                'Discovered PixelPusher on network: ',
+                controller.params.pixelpusher,
+                '-----------------------------------'];
+
+    info.forEach(line => debug(line));
+
+    // capture the update message sent back from the pp controller
+    controller.on('update', () => {
+      noise({ updatePeriod: this.params.pixelpusher.updatePeriod, deltaSequence: this.params.pixelpusher.deltaSequence, powerTotal: this.params.pixelpusher.powerTotal });
+    }).on('timeout', () => {
+      debug('TIMEOUT : PixelPusher at address [' + controller.params.ipAddress + '] with MAC (' + controller.params.macAddress + ') has timed out. Awaiting re-discovery....');
+      if (!!vm.timer) clearInterval(timer);
+    });
+
+    var NUM_STRIPS = controller.params.pixelpusher.numberStrips;
+    var STRIPS_PER_PACKET = controller.params.pixelpusher.stripsPerPkt;
+    var NUM_PACKETS_PER_UPDATE = NUM_STRIPS/STRIPS_PER_PACKET;
+    vm.PIXELS_PER_STRIP = controller.params.pixelpusher.pixelsPerStrip;
+
+    var waveHeight = vm.PIXELS_PER_STRIP/2;
+    var waveWidth = 2;
+    var wavePosition = 0;
+    vm.strip = new vm.PixelStrip(0, vm.PIXELS_PER_STRIP);
 
 
-console.log(PixelPusher);
+    vm.timer = setInterval(function() {
+      vm.exec();
+    }, vm.UPDATE_FREQUENCY_MILLIS);
+
+  }).on('error', (err) => {
+    debug('PixelPusher Error: ' + err.message);
+  });
+};
+
+
+var PixelPusherInterface = new PixelInterface();
+
+function oi() { debug('executable says oi!');}
+
+PixelPusherInterface.updateExecutable(oi);
+PixelPusherInterface.updateTiming(1);
+
+// Set the exec pattern
 
 
 var fs = require('fs');
