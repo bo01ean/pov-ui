ec240ce9868d9eb0e72419b4d563d87f1064c478
Nathan Trujillo <NathanTrujillo@shop.local>
2017-01-17 19:05:53 -0800

    Try to get alpha channel working.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var SerialPort = require('serialport');
 var PixelPusher = require('heroic-pixel-pusher');
-var getPixels = require("get-pixels");
+var getPixels = require('get-pixels');
 var fs = require('fs');
 var q = require('q');
 
@@ -28,8 +28,8 @@ var RPM;
 var rpmReg = new RegExp("\(smoothed\): ([0-9.]+)", "g");
 var tickReg = new RegExp("TICK", "g");
 var teensyReg = new RegExp("usb.+[0-9]+");
-var teensySearchPeriod = 1000;
 
+var teensySearchPeriod = 1000;
 var imageChangePeriod = 15000;
 
 var imageColumns = [];
@@ -50,7 +50,7 @@ var PixelInterface = function () {
   vm.PixelStrip = PixelPusher.PixelStrip;
   vm.PixelPusherInstance = new PixelPusher(); // will start listener.
 
-  vm.UPDATE_FREQUENCY_MILLIS = 10;
+  vm.UPDATE_FREQUENCY_MILLIS = 33;
   vm.PIXELS_PER_STRIP = 360;
   vm.exec = function () { return function () {} }; // NOP CLOSURE
 
@@ -130,22 +130,32 @@ var PixelInterface = function () {
     return function writeColumns () {
 
       columnIter = (columnIter >= imageColumns.length - 1) ? 0 : columnIter += 1;
-      //column = imageColumns[columnIter];
-
-      //debug('columnIter', columnIter, 'imageColumns.length', imageColumns.length, 'columnIter[0]', imageColumns[columnIter][0]);
+      column = imageColumns[columnIter];
+      //debug('imageColumns[columnIter].length/2',
+      //imageColumns[columnIter].length/2, 'columnIter', columnIter, 'imageColumns.length',
+      //imageColumns.length, 'imageColumns[' + columnIter + '][0, mid, last]',  imageColumns[columnIter][0], imageColumns[columnIter][imageColumns[columnIter].length/2], imageColumns[columnIter][imageColumns[columnIter].length - 1]);
 
 //      column.forEach((col,id) => { console.log(column, col);})
-/*
+
       for (var i = 0; i < vm.PIXELS_PER_STRIP; i++) {
         strips.forEach(function (strip, stripIndex) {
-          //debug('pixel', i, 'strip', stripIndex, column[i][0], column[i][1], column[i][2]);
-          strip.getPixel(i).setColor(column[i][0], column[i][1], column[i][2], 1.0);
+          // 0.0 = 255
+          // 1.0 = 1
+          //debug('pixel', i, 'strip', stripIndex, imageColumns[columnIter][i][0], imageColumns[columnIter][i][1], imageColumns[columnIter][i][2]);
+          debug(i, imageColumns[columnIter][i],
+            dec2bin(imageColumns[columnIter][i][3]),
+            dec2bin(imageColumns[columnIter][i][3]).replace(/./g,x=>x^1));
+          /*strip.getPixel(i).setColor(
+            imageColumns[columnIter][i][0],
+            imageColumns[columnIter][i][1],
+            imageColumns[columnIter][i][2],
+            imageColumns[columnIter][i][3] / 255
+          );*/
         });
       }
-      */
 
       strips.forEach(function (strip) {
-        strip.getRandomPixel().setColor(0, 0, 255, 0.1);
+        strip.getRandomPixel().setColor(0, 0, 255, 0.3);
       });
 
       vm.controller.emit('data', strips.map(function (strip) { return strip.getStripData();}));
@@ -188,8 +198,8 @@ var PixelInterface = function () {
     //var NUM_PACKETS_PER_UPDATE = NUM_STRIPS/STRIPS_PER_PACKET;
     vm.PIXELS_PER_STRIP = controller.params.pixelpusher.pixelsPerStrip;
 
-    vm.exec = vm.waveRider(stripsArray); // returns closure
-//    vm.exec = vm.writeImage(stripsArray);
+//    vm.exec = vm.waveRider(stripsArray); // returns closure
+    vm.exec = vm.writeImage(stripsArray);
 
     vm.timer = setInterval(function() {
       vm.exec();
@@ -233,11 +243,10 @@ function getNDColumns(pixels) {
   var width = pixels.shape[0];
   var height = pixels.shape[1];
   imageColumns = [];
-
   for (var col = 0; col < width; col++) {
     imageColumns[col] = [];
     for (var row = 0; row < height; row++) {
-      imageColumns[col].push([pixels.get(col, row, 0), pixels.get(col, row, 1), pixels.get(col, row, 2)]);
+      imageColumns[col].push([pixels.get(col, row, 0), pixels.get(col, row, 1), pixels.get(col, row, 2), pixels.get(col, row, 3)]);
     }
   }
 }
@@ -279,6 +288,10 @@ function pickupTeensy () {
       }
     });
   });
+}
+
+function dec2bin(dec){
+    return (dec >>> 0).toString(2);
 }
 
 setInterval(function () {
